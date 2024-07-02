28a78fc178f14d18322c1b259e1d5441f65b73bc
jamesmayke <jamezmayke28@gmail.com>
2024-07-02 08:16:04 -0300

    Testando Entidade Book

--- src/com/jamezmayke/readworld/modules/console/ReadWorld.java
+++ src/com/jamezmayke/readworld/modules/console/ReadWorld.java
@@ -1,7 +1,13 @@
 package src.com.jamezmayke.readworld.modules.console;
 
+import src.com.jamezmayke.readworld.modules.book.Book;
+import src.com.jamezmayke.readworld.modules.book.utils.Genre;
+
 public class ReadWorld {
     public static void main(String[] args) {
 
+        var oSenhorDosAneis = new Book("O senhor dos anéis: A sociedade do anel", Genre.SUSPENSE);
+
+        System.out.println(oSenhorDosAneis);
     }
 }
